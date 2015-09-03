1328e59d144f07a202169797c840ed1ed0a54a59
GitAllen <generic_hl777@163.com>
2015-09-03 04:11:23 +0000

    add web project

---
+++ VSLoadTestSample.Web/Controllers/LoadController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.Http;
+
+namespace Web.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class LoadController : ApiController
+    {
+        [HttpGet]
+        [Route("ip")]
+        public string ServerIp()
+        {
+            string result = string.Empty;
+            NetworkInterface[] nets = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var net in nets)
+            {
+                if (net.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    var ipProperties = net.GetIPProperties();
+                    if (ipProperties.UnicastAddresses.Count > 0)
+                    {
+                        foreach (var address in ipProperties.UnicastAddresses)
+                        {
+                            result += address.Address + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IHttpActionResult TestGet(string foo, string bar)
+        {
+            return Ok(foo + bar);
+        }
+
+        [HttpPost]
+        [Route("post")]
+        public IHttpActionResult TestPost(string foo, string bar)
+        {
+            return Ok(foo + bar);
+        }
+    }
+}
